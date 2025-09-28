d5ca331bb708bbcdd48a40b816521860eaaf990f
garcia <carlos.garcia3011@hotmail.com>
2025-09-28 15:54:28 -0600

    feat(user): integrate vanilo admin and user management
    - Register User model with Concord in AppServiceProvider
    - Add Vanilo packages to composer.json
    - Create concord.php config file
    - Implement UserContract in User model with activation methods

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Konekt\User\Contracts\Profile;
+use Konekt\User\Contracts\User as UserContract;
 
-class User extends Authenticatable
+class User extends Authenticatable implements UserContract
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -44,5 +46,22 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+     public function inactivate()
+    {
+        $this->is_active = false;
+        $this->save();
+    }
+
+    public function activate()
+    {
+        $this->is_active = true;
+        $this->save();
+    }
+
+    public function getProfile(): ?Profile
+    {
+        return null;
     }
 }
---
+++ config/concord.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'modules' => [
+        /**
+         * Example:
+         * VendorA\ModuleX\Providers\ModuleServiceProvider::class,
+         * VendorB\ModuleY\Providers\ModuleServiceProvider::class
+         *
+         */
+        Vanilo\Foundation\Providers\ModuleServiceProvider::class
+
+        
+    ],
+    'register_route_models' => true
+];
